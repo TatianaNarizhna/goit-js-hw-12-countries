073f676eaa3cebd8dd87eba0c0ea2e0ae06f7f43
TatianaNarizhna <narizhna.tania@gmail.com>
2021-05-28 18:15:37 +0300

    input

--- src/code.js/fetchCountries.js
+++ src/code.js/fetchCountries.js
@@ -1,19 +1,15 @@
 
+ 
  function fetchCountries(countryName) {
     
       return fetch(`https://restcountries.eu/rest/v2/name/${countryName}`)
       .then(response => {
         return response.json();
+    },
+  );
+}
 
-    })
-    // .then(name => {
-    //     console.log(name);
-        
-    // })
-
-};
-
-// export default { fetchCountries };
+export default { fetchCountries };
 
 // fetch(`https://restcountries.eu/rest/v2/name/${name}`)
 // .then(res => res.json())
--- src/code.js/main.js
+++ src/code.js/main.js
@@ -1,9 +1,53 @@
 
+import countryTmpl from '../templates/country.hbs';
 
-import countries from '../templates/countries.hbs';
-import country from '../templates/country.hbs';
+// import debounce from 'lodash.debounce';
+import countriesTmpl from '../templates/countries.hbs';
 
-import ApiCountries from "./fetchCountries";
+import ApiCountries from "./fetchCountries.js";
+
+const debounce = require('lodash.debounce');
+
+
+const refs = {
+    input: document.querySelector('.input-control'),
+    flagContainer: document.querySelector('.js-country-container'),
+}
+
+refs.input.addEventListener('input', debounce(onInputCountry, 500));
+
+function onInputCountry(e) {
+    let countryName = e.target.value;
+
+    ApiCountries.fetchCountries(countryName)
+    .then(country => {
+        if(country.length < 2) {
+            cleanInput();
+            renderCountry(country);
+            
+        } else if(country.length > 1 && country.length <= 10) {
+            cleanInput();
+            renderCountries(country);
+            
+        }
+    })
+}
+
+function renderCountry (country) { 
+    const countryMarkUp = countryTmpl(country);
+    refs.flagContainer.insertAdjacentHTML('beforeend', countryMarkUp);
+}
+
+function renderCountries (countries) {
+    const countriesMarkUp = countriesTmpl(countries);
+    refs.flagContainer.insertAdjacentHTML('beforeend', countriesMarkUp);
+    
+}
+
+function cleanInput() {
+    refs.flagContainer.innerHTML = '';
+}
+
 
 
 
